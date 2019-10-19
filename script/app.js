f8612bff167328616aaf6272cff0a3407f72c9dc
cedlog <cedlog@dev.local>
2019-10-19 18:50:48 +0000

    Equalize heights when window is resized

--- script/app.js
+++ script/app.js
@@ -1,6 +1,9 @@
-body = document.body;
-side = document.getElementById("side")
-content = document.getElementById("content")
-maxHeight = Math.max(body.offsetHeight, side.offsetHeight, content.offsetHeight)
-side.style.height = content.style.height = maxHeight + "px"
-console.info("Height updated: " + maxHeight)
+window.onresize = window.onload = function () {
+   root = document.documentElement;
+   side = document.getElementById("side");
+   content = document.getElementById("content");
+   root.style.height = side.style.height = content.style.height = "auto";
+   maxHeight = Math.max(root.scrollHeight, side.scrollHeight, content.scrollHeight);
+   root.style.height = side.style.height = content.style.height = maxHeight + "px";
+   console.debug("Height updated: " + maxHeight);
+};
